3e8baa5ceb5f4536af7f0ae26e82fb6d20614b47
Phil Huang <hphil@vmware.com>
2021-10-01 13:02:46 +0800

    Add Login Tanzu Kubernetes Cluster Method
    Signed-off-by: Phil Huang <hphil@vmware.com>

---
+++ 1-login-tanzu-kubernetes-cluster.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+kubectl vsphere login --server=${SUPERVISOR_CLUSTER_CONTROL_PLANE_IP} \
+--vsphere-username ${VCENTER_SSO_USER_NAME} \
+--insecure-skip-tls-verify
+
+kubectl config get-contexts
